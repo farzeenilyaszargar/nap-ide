dd20f9b7acb672d9367a04232af7db80c414be8d
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2026-03-05 00:01:03 +0530

    Simplify download button to only show Apple icon

--- components/customDownload.tsx
+++ components/customDownload.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useMemo } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,15 +7,6 @@ type DownloadButtonProps = {
 };
 
 export default function DownloadButton({ className = "" }: DownloadButtonProps) {
-  const osIcon = useMemo(() => {
-    if (typeof navigator === "undefined") return "/download.png";
-    const ua = navigator.userAgent.toLowerCase();
-    if (ua.includes("mac os") || ua.includes("macintosh")) return "/apple-icon.png";
-    if (ua.includes("windows")) return "/windows-icon.png";
-    if (ua.includes("linux")) return "/linux-icon.png";
-    return "/download.png";
-  }, []);
-
   return (
     <Link
       href="/download"
@@ -24,18 +14,11 @@ export default function DownloadButton({ className = "" }: DownloadButtonProps) 
     >
       <span>Download Now</span>
       <Image
-        src={osIcon}
+        src="/apple-icon.png"
         width={16}
         height={16}
         alt="platform icon"
         className="h-4 w-4 object-contain"
-      />
-      <Image
-        src="/right-arrow.png"
-        width={16}
-        height={16}
-        alt="arrow"
-        className="h-3.5 w-3.5 rotate-[320deg] invert"
       />
     </Link>
   );
