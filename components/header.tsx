c29a33b2f54464a5063372906516c2cebce9208c
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2026-03-05 00:08:47 +0530

    Reinforce sticky navbar positioning and layering

--- components/header.tsx
+++ components/header.tsx
@@ -87,7 +87,7 @@ export default function Header() {
 
     return (
         <>
-            <header className="sticky top-0 z-40 w-full bg-white">
+            <header className="sticky top-0 left-0 z-50 w-full bg-white">
                 <div className="mx-auto flex h-16 w-full max-w-7xl items-center justify-between px-5 sm:px-10 lg:px-14">
                     <Link href="/" className="flex items-center gap-2.5">
                         <Image src="/logo.png" alt="Nap" width={36} height={20} className="h-5 w-auto rounded-md object-contain" />
