630e7b8e58e48d11471f82b22509841b92c5bee7
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2026-03-11 01:59:10 +0530

    Use logo-white.ico as favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   description: "The Best Way To Code With AI",
   keywords: ["ide", "dev tool", "ai dev tools", "ai ide", "ai developement", "developement platform", "developement tool", "nap", "editor", "nap editor"],
   alternates: { canonical: "www.nap.bot" },
-  icons: { icon: "/logo.ico" },
+  icons: { icon: "/logo-white.ico" },
   robots: {
     index: true,
     follow: true,
