58fc6189982b2d3e300317ae8e2c29983602f150
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2026-03-13 12:40:56 +0530

    Widen navbar container

--- components/header.tsx
+++ components/header.tsx
@@ -87,7 +87,7 @@ export default function Header() {
     return (
         <>
             <header className="sticky top-0 z-20 w-full bg-transparent backdrop-blur-md">
-                <div className="mx-auto flex h-14 w-full max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-10">
+                <div className="mx-auto flex h-14 w-full max-w-[88rem] items-center justify-between px-3 sm:px-4 lg:px-6">
                 <Link href="/" className="flex justify-center items-center">
                     <Image src="/logo.png" alt="Nap" width={40} height={40} className="h-4 w-auto rounded-md sm:h-5" />
                 </Link>
