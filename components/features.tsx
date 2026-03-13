b8a8cde68c961623614579f43053ce45fb5b0132
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2026-03-13 13:08:51 +0530

    Increase feature header size

--- components/features.tsx
+++ components/features.tsx
@@ -6,13 +6,13 @@ export default function Features() {
 
             <div className="flex flex-col md:flex-row gap-6 sm:gap-4">
                 <div className="flex flex-col mx-4 sm:m-10 bg-[#F5F5F5] rounded-2xl p-6 sm:p-15 gap-6 sm:gap-15 text-center">
-                    <p className="text-lg sm:text-3xl font-bold text-[#7E7E7E]">For the <span className="text-[#414141]">real devs</span>  <br></br>Nap is the best way to <span className="text-[#414141]">code.</span></p>
+                    <p className="text-xl sm:text-4xl font-bold text-[#7E7E7E]">For the <span className="text-[#414141]">real devs</span>  <br></br>Nap is the best way to <span className="text-[#414141]">code.</span></p>
                     <p className="text-xs sm:text-base">For developers who builds real stuff nap delivers a world-class intelligent agent that sees your entire codebase with absolute clarity, understands every file, every nuance, and builds with the sophistication of a top-tier engineer.</p>
                     <Image src="/parallel.jpeg" width={2000} height={2000} alt="img" className="rounded-2xl" />
                 </div>
                 <div className="flex flex-col mx-4 sm:m-10 bg-[#1A1A1A] rounded-2xl p-6 sm:p-15 gap-6 sm:gap-15 text-center">
                     <Image src="/files.jpeg" width={2000} height={2000} alt="img" className="rounded-2xl" />
-                    <p className="text-lg sm:text-3xl font-bold text-[#EAEAEA]">Nap edits <span className="text-[#6D6D6D]">multiple files at a time</span>  so relax <span className="text-[#6D6D6D]">&amp; take a nap.</span></p>
+                    <p className="text-xl sm:text-4xl font-bold text-[#EAEAEA]">Nap edits <span className="text-[#6D6D6D]">multiple files at a time</span>  so relax <span className="text-[#6D6D6D]">&amp; take a nap.</span></p>
                     <p className="text-xs sm:text-base text-[#6D6D6D]">Manage agents, workflows, and project state from one place. Switch contexts fast, track changes, and keep the entire build loop under your control.</p>
                 </div>
             </div>
