ae6804645538c99f78175571ccb85820f718dddb
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2026-03-18 12:51:55 +0530

    Remove host redirect from Next config

--- next.config.ts
+++ next.config.ts
@@ -15,16 +15,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.nap-code.com" }],
-        destination: "https://nap-code.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
