63ab6c9afe4f56fb3569856e8b0da032bcc239e7
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2026-03-20 13:34:47 +0530

    Lighten CTA headline weight

--- components/trySurfer.tsx
+++ components/trySurfer.tsx
@@ -4,7 +4,7 @@ import DownloadButton from "./customDownload";
 export default function TryNap() {
     return (
         <div className="mx-auto flex w-full max-w-7xl flex-col items-center justify-center gap-8 px-4 py-16 sm:gap-10 sm:px-6 sm:py-24 lg:px-10">
-            <p className="text-center text-4xl font-medium sm:text-7xl text-[#131313]">Start Building Now</p>
+            <p className="text-center text-4xl font-normal sm:text-7xl text-[#131313]">Start Building Now</p>
             <DownloadButton />
 
         </div>
