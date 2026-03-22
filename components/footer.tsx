b426c2c031558706c0376545e1f188a0f8a97892
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2026-03-22 12:18:05 +0530

    Set upper footer text to base size

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,7 +9,7 @@ const wixMadeforText = Wix_Madefor_Text({
 export default function Footer() {
     return (
         <footer className={`w-full px-5 py-2 text-gray-300 sm:px-5 lg:px-10 tracking-[-0.015em] text-sm ${wixMadeforText.className} flex flex-col items-center justify-center`}>
-            <div className="mx-auto flex w-full max-w-6xl flex-col items-center justify-center gap-2 text-gray-400 py-2 text-center">
+            <div className="mx-auto flex w-full max-w-6xl flex-col items-center justify-center gap-2 text-base text-gray-400 py-2 text-center">
                 <p className="text-center">
                     For anything specific about Nap email us at{" "}
                     <a className="underline underline-offset-2 text-black hover:text-gray-500" href="mailto:issues@nap-code.com">issues@nap-code.com</a>
@@ -27,8 +27,6 @@ export default function Footer() {
                     <Link className="whitespace-nowrap transition-colors hover:text-black" href="/tc">Terms &amp; Conditions</Link>
                     <span className="inline-flex items-center leading-none ">|</span>
                     <Link className="whitespace-nowrap transition-colors hover:text-black" href="/data-usage">Data Usage</Link>
-                    <span className="inline-flex items-center leading-none ">|</span>
-                    <Link className="whitespace-nowrap transition-colors hover:text-black" href="/reachus">Reach Us</Link>
                 </div>
                 <span>8 Off Western Road, 400068, Mumbai, India.</span>
             </div>
