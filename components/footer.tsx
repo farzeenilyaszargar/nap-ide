b63173b770ea46e3630992434b2fc8409eb67420
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2026-03-22 12:10:17 +0530

    Set footer link colors to black with gray hover

--- components/footer.tsx
+++ components/footer.tsx
@@ -12,11 +12,11 @@ export default function Footer() {
             <div className="mx-auto flex w-full max-w-6xl flex-col items-center justify-between gap-2 text-xs text-gray-400 sm:flex-row sm:text-sm py-2 text-center">
                 <p className="text-center">
                     For anything specific about Nap email us at{" "}
-                    <a className="underline underline-offset-2 hover:text-black" href="mailto:issues@nap-code.com">issues@nap-code.com</a>
-                    {" "}or you can directly contact us on +91 8855885588 (Nap’s help desk ). Check us on social media on{" "}
-                    <Link className="underline underline-offset-2 hover:text-black" href="https://x.com/napverse" target="_blank">X</Link>
+                    <a className="underline underline-offset-2 text-black hover:text-gray-500" href="mailto:issues@nap-code.com">issues@nap-code.com</a>
+                    {" "}. Check us on social media on{" "}
+                    <Link className="underline underline-offset-2 text-black hover:text-gray-500" href="https://x.com/napverse" target="_blank">X</Link>
                     {" "} &amp;{" "}
-                    <Link className="underline underline-offset-2 hover:text-black" href="https://www.youtube.com/@napHQ" target="_blank">YouTube</Link>.
+                    <Link className="underline underline-offset-2 text-black hover:text-gray-500" href="https://www.youtube.com/@napHQ" target="_blank">YouTube</Link>.
                 </p>
             </div>
             <div className="mx-auto flex w-full max-w-6xl flex-col items-center justify-between gap-2 text-xs text-gray-400 sm:flex-row sm:text-xs  border-t border-gray-200 py-2">
