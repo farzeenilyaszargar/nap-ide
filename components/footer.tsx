991ef254f6d043e6a3dabc327daf7a3e8593dc83
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2026-03-22 16:45:03 +0530

    popsicle

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,7 +9,7 @@ const wixMadeforText = Wix_Madefor_Text({
 export default function Footer() {
     return (
         <footer className={`w-full px-5 py-2 text-gray-300 sm:px-5 lg:px-10 tracking-[-0.015em] text-xs ${wixMadeforText.className} flex flex-col items-center justify-center`}>
-            <div className="mx-auto flex w-full max-w-6xl flex-col items-center whitespace-nowrap justify-center gap-2 text-xs text-gray-400 py-2 text-center tracking-[-0.015em]">
+            <div className="mx-auto flex w-full max-w-5xl flex-col items-center whitespace-nowrap justify-center gap-2 text-xs text-gray-400 py-2 text-center tracking-[-0.015em]">
                 <p className="text-center">
                     For anything specific about Nap or any other issues, email us at{" "}
                     <a className="underline underline-offset-2 text-black hover:text-gray-500" href="mailto:issues@nap-code.com">issues@nap-code.com</a>
@@ -17,11 +17,15 @@ export default function Footer() {
                     <Link className="underline underline-offset-2 text-black hover:text-gray-500" href="https://x.com/napverse" target="_blank">X</Link>
                     {" "} &amp;{" "}
                     <Link className="underline underline-offset-2 text-black hover:text-gray-500" href="https://www.youtube.com/@napHQ" target="_blank">YouTube</Link>.
+                    Enjoy coding and building projects. Creare est vivere!
                 </p>
             </div>
-            <div className="mx-auto flex w-full max-w-6xl flex-col items-center justify-between gap-2 text-xs text-gray-400 sm:flex-row border-t border-gray-200 py-2 tracking-[-0.015em]">
+            <div className="mx-auto flex w-full max-w-5xl flex-col items-center justify-between gap-2 text-xs text-gray-400 sm:flex-row border-t border-gray-200 py-2 tracking-[-0.015em]">
                 <span className="whitespace-nowrap">Copyright © 2026 Nap Inc. All Rights Reserved.</span>
                 <div className="hidden flex-wrap items-center justify-center gap-2 sm:flex sm:justify-end">
+                    <Link className="whitespace-nowrap transition-colors hover:text-black" href="/download">Download</Link>
+                    <span className="inline-flex items-center leading-none ">|</span>
+
                     <Link className="whitespace-nowrap transition-colors hover:text-black" href="/privacy">Privacy Policy</Link>
                     <span className="inline-flex items-center leading-none ">|</span>
                     <Link className="whitespace-nowrap transition-colors hover:text-black" href="/tc">Terms &amp; Conditions</Link>
