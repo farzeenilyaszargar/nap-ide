1f93d79b2814358f8838e06be5a162c9a3221851
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2026-03-22 12:06:40 +0530

    Link footer email and social mentions

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,12 +9,17 @@ const wixMadeforText = Wix_Madefor_Text({
 export default function Footer() {
     return (
         <footer className={`w-full px-5 text-gray-300 sm:px-5 lg:px-10 tracking-[-0.015em] ${wixMadeforText.className}`}>
-            <div>
-                <p>
-                    For anything specific about Nap email us at issues@napeditor.com or you can directly contact us on +91 8855885588 (Nap’s help desk ). Check us on social media on X & Reddit.
+            <div className="mx-auto flex w-full max-w-6xl flex-col items-center justify-between gap-2 text-xs text-gray-400 sm:flex-row sm:text-xs py-3 text-center">
+                <p className="text-center">
+                    For anything specific about Nap email us at{" "}
+                    <a className="underline underline-offset-2 hover:text-black" href="mailto:issues@nap-code.com">issues@nap-code.com</a>
+                    {" "}or you can directly contact us on +91 8855885588 (Nap’s help desk ). Check us on social media on{" "}
+                    <Link className="underline underline-offset-2 hover:text-black" href="https://x.com/napverse" target="_blank">X</Link>
+                    {" "} &amp;{" "}
+                    <Link className="underline underline-offset-2 hover:text-black" href="https://www.youtube.com/@napHQ" target="_blank">YouTube</Link>.
                 </p>
             </div>
-            <div className="mx-auto flex w-full max-w-6xl flex-col items-center justify-between gap-2 text-xs text-gray-400 sm:flex-row sm:text-sm  border-t border-gray-200 py-4">
+            <div className="mx-auto flex w-full max-w-6xl flex-col items-center justify-between gap-2 text-xs text-gray-400 sm:flex-row sm:text-xs  border-t border-gray-200 py-3">
                 <span className="whitespace-nowrap">Copyright © 2026 Nap Inc. All Rights Reserved.</span>
                 <div className="hidden flex-wrap items-center justify-center gap-2 sm:flex sm:justify-end">
                     <Link className="whitespace-nowrap transition-colors hover:text-black" href="/privacy">Privacy Policy</Link>
