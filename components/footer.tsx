f03aae00a5d264892a62e98afd057134190e87be
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2026-03-22 12:34:32 +0530

    Set footer text to xs with tighter tracking

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,18 +8,18 @@ const wixMadeforText = Wix_Madefor_Text({
 
 export default function Footer() {
     return (
-        <footer className={`w-full px-5 py-2 text-gray-300 sm:px-5 lg:px-10 tracking-[-0.015em] text-base ${wixMadeforText.className} flex flex-col items-center justify-center`}>
-            <div className="mx-auto flex w-full max-w-6xl flex-col items-center justify-center gap-2 text-base text-gray-400 py-2 text-center">
+        <footer className={`w-full px-5 py-2 text-gray-300 sm:px-5 lg:px-10 tracking-[-0.015em] text-xs ${wixMadeforText.className} flex flex-col items-center justify-center`}>
+            <div className="mx-auto flex w-full max-w-6xl flex-col items-center whitespace-nowrap justify-center gap-2 text-xs text-gray-400 py-2 text-center tracking-[-0.015em]">
                 <p className="text-center">
-                    For anything specific about Nap email us at{" "}
+                    For anything specific about Nap or any other issues, email us at{" "}
                     <a className="underline underline-offset-2 text-black hover:text-gray-500" href="mailto:issues@nap-code.com">issues@nap-code.com</a>
-                    {" "}. Check us on social media on{" "}
+                    {" "}. Check us out on social media on{" "}
                     <Link className="underline underline-offset-2 text-black hover:text-gray-500" href="https://x.com/napverse" target="_blank">X</Link>
                     {" "} &amp;{" "}
                     <Link className="underline underline-offset-2 text-black hover:text-gray-500" href="https://www.youtube.com/@napHQ" target="_blank">YouTube</Link>.
                 </p>
             </div>
-            <div className="mx-auto flex w-full max-w-6xl flex-col items-center justify-between gap-2 text-base text-gray-400 sm:flex-row border-t border-gray-200 py-2">
+            <div className="mx-auto flex w-full max-w-6xl flex-col items-center justify-between gap-2 text-xs text-gray-400 sm:flex-row border-t border-gray-200 py-2 tracking-[-0.015em]">
                 <span className="whitespace-nowrap">Copyright © 2026 Nap Inc. All Rights Reserved.</span>
                 <div className="hidden flex-wrap items-center justify-center gap-2 sm:flex sm:justify-end">
                     <Link className="whitespace-nowrap transition-colors hover:text-black" href="/privacy">Privacy Policy</Link>
